cc15f3f7daa106f2c7b9c878d6cf96daacd9cb9c
Arshpreet Singh Bedi <arshbedi2517@gmail.com>
2025-04-05 17:41:43 +0530

    final changes

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,10 @@
 // for profile/abc -> individual user
 
-export default function UserProfile({ params }: { params: { id: string } }) {
+export default async function UserProfile({
+  params,
+}: {
+  params: { id: string };
+}) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
